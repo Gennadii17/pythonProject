84c3253c81673efd5fb1748f6197f0dfa5cb46b0
hennadiyzharkov <shvarzkopff@gmail.com>
2022-02-04 00:41:55 +0200

    Add task 3

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,1 +1,7 @@
+line_long = "#" * 9
+line_short = "#\t\t#"
+symbol_O = [line_long, line_short, line_short, line_short, line_long]
+symbol_H = [line_short, line_short, line_long, line_short, line_short]
 
+print(*symbol_O, sep="\n", end="\n\n")
+print(*symbol_H, sep="\n")
