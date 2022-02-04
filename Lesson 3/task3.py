9988ce790211feffd5bbc8aa7db185dae7368d00
hennadiyzharkov <shvarzkopff@gmail.com>
2022-02-05 00:15:16 +0200

    Added tasks for lesson 3

--- Lesson 3/task3.py
+++ Lesson 3/task3.py
@@ -1,3 +1,4 @@
 a = 10
 b = 5
-print(a+b, a-b, a/b, a*b, a**b, abs(b-a), a//b, sep="\n")
+print(f"Addition {a+b}", f"Subtraction {a-b}", f"Division {a/b}", f"Multiplication {a*b}",
+      f"Exponent {a**b}", f"Modulus {abs(b-a)}", f"Floor division {a//b}", sep="\n")
